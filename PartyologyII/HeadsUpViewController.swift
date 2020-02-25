9296844c82b900333f5416ed9107109ff7b294ae
Alex Levinson <s013227@students.lmsd.org>
2020-02-25 07:53:12 -0500

    Gyroscope stuff for Heads Up

--- PartyologyII/HeadsUpViewController.swift
+++ PartyologyII/HeadsUpViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import CoreMotion
 
 class HeadsUpViewController: UIViewController {
-
+    
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        accelerometer(false)
+        
+        
+        
+        view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func accelerometer(_ animated: Bool){
+        motionManager.accelerometerUpdateInterval = 0.2
+        
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) {(data, error) in if let myData = data{
+            //                    print(myData)
+            
+            if myData.acceleration.z > 0.6{
+                self.view.backgroundColor = UIColor.red
+            }
+            else if myData.acceleration.z < -0.6{
+                self.view.backgroundColor = UIColor.green
+            }
+            else{
+                self.view.backgroundColor = UIColor.white
+            }
+            }
+            
+        }
     }
-    */
+    
+    func gyroscope(_ animated: Bool){
+        motionManager.gyroUpdateInterval = 0.2
+        //        var movement = true
+        
+        motionManager.startGyroUpdates(to: OperationQueue.current!) {(data, error) in if let myData = data{
+            
+            if myData.rotationRate.y > 1.75{
+                print("Rotate back")
+                print(myData.rotationRate.y)
+                self.view.backgroundColor = UIColor.red
+            }
+            else if myData.rotationRate.y < -1.75{
+                print("Rotate forward")
+                print(myData.rotationRate.y)
+                self.view.backgroundColor = UIColor.green
+            }
+            
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
 
+                
+            }
+            
+            }
+        }
+        
+    }
+    
 }

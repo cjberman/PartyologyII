5cf8d2b3ace392bdabe388e49c4d3410a2903731
Alex Levinson <alex.levinson13@gmail.com>
2020-03-18 14:03:43 -0400

    coming together

--- PartyologyII/HeadsUpViewController.swift
+++ PartyologyII/HeadsUpViewController.swift
@@ -12,41 +12,84 @@ import CoreMotion
 class HeadsUpViewController: UIViewController {
     
     //instantiate deck
-    let deck = Deck()
+    let deck = Deck(c: [FlashCard("Basketball", "A"), FlashCard("Giraffe", "B"), FlashCard("Skiing", "C"),FlashCard("Handshake", "D") ], n: "Random Things")
+    
     let termLabel = UILabel()
+    let pointLabel = UILabel()
+    let timeLabel = UILabel()
+    
+    var termWasChanged = false
+    var points = 0
+    var timer = Timer()
+    var time = 0
+    
     var motionManager = CMMotionManager()
     
     
-    //set up label
-    func setUptermLabel(){
+    
+    
+    //viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        accelerometer(false)
+        
+        setUpTermLabel()
+        setUpPointLabel()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HeadsUpViewController.timeAction), userInfo: nil, repeats: true)
+        view.backgroundColor = UIColor.white
+    }
+        
+    @objc func timeAction(){
+        time += 1
+        timeLabel.text = String(time)
+    }
+    
+    override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+       return .landscape
+    }
+    
+    
+    
+    //set up termLabel
+    func setUpTermLabel(){
         //adding to view
         view.addSubview(termLabel)
         
         //setting up properties
         
-        termLabel.text = "Placeholder"
+        if let card = deck.getRandomCard(){
+            termLabel.text = card.term
+        } else{
+            termLabel.text = "error: deck contains null values"
+        }
         termLabel.textColor = UIColor.black
-        termLabel.font = UIFont(name: "Helvetica Neue", size: 30)
+        termLabel.font = UIFont(name: "Helvetica Neue", size: 64)
         
         //constraints
         termLabel.translatesAutoresizingMaskIntoConstraints = false
         termLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        termLabel.centerYAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
-    }
-        
-    //viewDidLoad
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        accelerometer(false)
-        
-        setUptermLabel()
-        
-        view.backgroundColor = UIColor.white
+        termLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
     }
         
         
+    func setUpPointLabel(){
+        //adding to view
+        view.addSubview(pointLabel)
         
+        //setting up properties
+        pointLabel.text = "\(points)"
+        pointLabel.textColor = UIColor.black
+        pointLabel.font = UIFont(name: "Helvetica Neue", size: 64)
+        
+        //constraints
+        pointLabel.translatesAutoresizingMaskIntoConstraints = false
+        pointLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        pointLabel.bottomAnchor.constraint(equalTo: termLabel.bottomAnchor, constant: 100).isActive = true
+    }
+        
+    
+    
         
     func accelerometer(_ animated: Bool){
         motionManager.accelerometerUpdateInterval = 0.2
@@ -56,11 +99,25 @@ class HeadsUpViewController: UIViewController {
             
             if myData.acceleration.z > 0.6{
                 self.view.backgroundColor = UIColor.red
+                
+                if(!self.termWasChanged){
+                    self.termLabel.text = self.deck.getRandomCard()?.term
+                    self.termWasChanged = true
+                }
+                
             }
             else if myData.acceleration.z < -0.6{
                 self.view.backgroundColor = UIColor.green
+                
+                if(!self.termWasChanged){
+                    self.termLabel.text = self.deck.getRandomCard()?.term
+                    self.points += 100
+                    self.setUpPointLabel()
+                    self.termWasChanged = true
+                }
             }
             else{
+                self.termWasChanged = false
                 self.view.backgroundColor = UIColor.white
             }
             }

8005402fe53e8ebfe4c9eb1a247cdb7217a447b1
Alex Levinson <alex.levinson13@gmail.com>
2020-03-18 15:01:41 -0400

    Finished Heads Up + comments

--- PartyologyII/HeadsUpViewController.swift
+++ PartyologyII/HeadsUpViewController.swift
@@ -11,18 +11,23 @@ import CoreMotion
 
 class HeadsUpViewController: UIViewController {
     
-    //instantiate deck
+    //declarations
+    //test deck - will be eventually replaced
     let deck = Deck(c: [FlashCard("Basketball", "A"), FlashCard("Giraffe", "B"), FlashCard("Skiing", "C"),FlashCard("Handshake", "D") ], n: "Random Things")
-    
+    //labels & button
     let termLabel = UILabel()
     let pointLabel = UILabel()
     let timeLabel = UILabel()
+    let exitButton = UIButton()
     
+    //termWasChanged - used for ensuring the term is only changed once when device is tilted
     var termWasChanged = false
+    //points - keeps track of points
     var points = 0
     var timer = Timer()
-    var time = 0
-    
+    //time - starts at 60, decreases by 1 every second
+    var time = 60
+    //motionManager - needed for accelerometer
     var motionManager = CMMotionManager()
     
     
@@ -31,27 +36,48 @@ class HeadsUpViewController: UIViewController {
     //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        //calls acceleromter
         accelerometer(false)
         
+        //sets up labels & button
         setUpTermLabel()
         setUpPointLabel()
+        setUpTimeLabel()
+        setUpExitButton()
         
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HeadsUpViewController.timeAction), userInfo: nil, repeats: true)
         view.backgroundColor = UIColor.white
-    }
         
+        //sets up timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(HeadsUpViewController.timeAction), userInfo: nil, repeats: true)
+        
+    }
+    
+    //called every second
     @objc func timeAction(){
-        time += 1
+        //decreses time and updates timeLabel
+        time -= 1
         timeLabel.text = String(time)
+        
+        //game over sequence
+        if (time <= 0){
+            view.backgroundColor = UIColor.black
+            termLabel.text = "GAME OVER"
+            termLabel.textColor = UIColor.white
+            pointLabel.textColor = UIColor.white
+            exitButton.setTitleColor(UIColor.white, for: .normal)
+        }
+        
+        
     }
     
+    //locks app in landscape mode
     override open var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-       return .landscape
+        return .landscape
     }
     
     
     
-    //set up termLabel
+    //set up labels & button
     func setUpTermLabel(){
         //adding to view
         view.addSubview(termLabel)
@@ -71,8 +97,8 @@ class HeadsUpViewController: UIViewController {
         termLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         termLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
     }
-        
-        
+    
+    
     func setUpPointLabel(){
         //adding to view
         view.addSubview(pointLabel)
@@ -87,41 +113,81 @@ class HeadsUpViewController: UIViewController {
         pointLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
         pointLabel.bottomAnchor.constraint(equalTo: termLabel.bottomAnchor, constant: 100).isActive = true
     }
-        
     
-    
+    func setUpTimeLabel(){
+        //adding to view
+        view.addSubview(timeLabel)
         
+        //setting up properties
+        timeLabel.text = "60"
+        timeLabel.textColor = UIColor.black
+        timeLabel.font = UIFont(name: "Helvetica Neue", size: 64)
+        
+        //constraints
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+    }
+    
+    func setUpExitButton(){
+        //adding to view
+        view.addSubview(exitButton)
+        
+        //setting up properties
+        exitButton.setTitle("X", for: .normal)
+        exitButton.setTitleColor(UIColor.black, for: .normal)
+        exitButton.addTarget(self, action: #selector(exit), for: .touchUpInside)
+        exitButton.titleLabel?.font = UIFont(name: "Helvetica Neue", size: 64)
+        
+        //constraints
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        exitButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        exitButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+    }
+    
+    //called when exitButton is pressed
+    @objc func exit(){
+        //segue here
+    }
+    
     func accelerometer(_ animated: Bool){
-        motionManager.accelerometerUpdateInterval = 0.2
         
+        motionManager.accelerometerUpdateInterval = 0.2
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!) {(data, error) in if let myData = data{
-            //                    print(myData)
             
-            if myData.acceleration.z > 0.6{
-                self.view.backgroundColor = UIColor.red
+            //turns off accelerometer once time is up
+            if (self.time > 0){
                 
-                if(!self.termWasChanged){
-                    self.termLabel.text = self.deck.getRandomCard()?.term
-                    self.termWasChanged = true
+                //sequence when device is tilted down
+                if myData.acceleration.z > 0.6{
+                    self.view.backgroundColor = UIColor.red
+                    
+                    //updates term
+                    if(!self.termWasChanged){
+                        self.termLabel.text = self.deck.getRandomCard()?.term
+                        self.termWasChanged = true
+                    }
+                    
                 }
-                
-            }
-            else if myData.acceleration.z < -0.6{
-                self.view.backgroundColor = UIColor.green
-                
-                if(!self.termWasChanged){
-                    self.termLabel.text = self.deck.getRandomCard()?.term
-                    self.points += 100
-                    self.setUpPointLabel()
-                    self.termWasChanged = true
+                //sequence when device is tilted up
+                else if myData.acceleration.z < -0.6{
+                    self.view.backgroundColor = UIColor.green
+                    
+                    //updates term and points
+                    if(!self.termWasChanged){
+                        self.termLabel.text = self.deck.getRandomCard()?.term
+                        self.points += 100
+                        self.setUpPointLabel()
+                        self.termWasChanged = true
+                    }
+                }
+                //sequence when device is returned to neutral
+                else{
+                    self.termWasChanged = false
+                    self.view.backgroundColor = UIColor.white
                 }
             }
-            else{
-                self.termWasChanged = false
-                self.view.backgroundColor = UIColor.white
             }
-            }
-            
         }
     }
     

07a43ecdf8c0ef0dc4b1974f8e50405fd583118e
Charlie Berman <s022033@students.lmsd.org>
2020-03-24 22:22:19 -0400

    Trying something new in pullDeck()

--- PartyologyII/DeckEditViewController.swift
+++ PartyologyII/DeckEditViewController.swift
@@ -58,10 +58,6 @@ class DeckEditViewController: UIViewController {
             dictionary[i.term] = i.definition
         }
         
-        for i in deck.cards{
-            dictionary[i.term] = i.definition
-        }
-        
         //updates database with the dictionary
         ref?.child("Decks").child(deck.name).setValue(dictionary)
         
@@ -69,30 +65,40 @@ class DeckEditViewController: UIViewController {
     
     //pulls all the cards in a deck when updated (so if a change occurs to any deck, will reupdate I think), returns a deck
     func pullDeck(){
-        let jsonURLString = "https://partyologyii.firebaseio.com/"
-               
-        guard let url = URL(string: jsonURLString) else {return}
+//        let jsonURLString = "https://partyologyii.firebaseio.com/"
+//
+//        guard let url = URL(string: jsonURLString) else {return}
+//
+//        print("Entering pullDeck function")
+//        //Use the URLSession singleton to perform a dataTask. After dataTask gets the informaton from our URL, it will call the closure, passing it data, response, err. .resume() starts our dataTask
+//        URLSession.shared.dataTask(with: url) { (data, response, err) in
+//        guard let data = data else {return}
+//
+//             print("Made it into URLSession")
+//
+//            do{
+//                self.deckDecodable = try JSONDecoder().decode(DataData.self, from: data)
+//                print(self.deckDecodable as Any)
+//                print("Made it into do part")
+//                DispatchQueue.main.async {
+//                    print("ASYNC")
+//                }
+//            }catch let jsonErr{
+//                print(jsonErr)
+//                print("do part failed")
+//            }
+//            }.resume()
+//    }
         
-        print("Entering pullDeck function")
-        //Use the URLSession singleton to perform a dataTask. After dataTask gets the informaton from our URL, it will call the closure, passing it data, response, err. .resume() starts our dataTask
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-        guard let data = data else {return}
+        ref?.child("Decks").observeSingleEvent(of: .value, with: { (snapshot) in
+          // Get user value
+          let value = snapshot.value as? NSDictionary
+            print(value)
 
-             print("Made it into URLSession")
-            
-            do{
-                self.deckDecodable = try JSONDecoder().decode(DataData.self, from: data)
-                print(self.deckDecodable as Any)
-                print("Made it into do part")
-                DispatchQueue.main.async {
-                    print("ASYNC")
-                }
-            }catch let jsonErr{
-                print(jsonErr)
-                print("do part failed")
-            }
-            }.resume()
-    }
+          // ...
+          }) { (error) in
+            print(error.localizedDescription)
+        }
     
     
 
@@ -100,3 +106,4 @@ class DeckEditViewController: UIViewController {
 
 }
 
+}
